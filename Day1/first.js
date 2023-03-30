d20ecb319f29e241720968e0c3b9956966262fa6
ishan <sethiishan19@gmail.com>
2023-03-30 22:49:17 +0530

    Javascript Basics

---
+++ Day1/first.js
@@ -0,0 +1,15 @@
+// Single Line comment
+/*
+Multiple Line comments
+*/
+
+//variable in javascript
+
+number=1;
+console.log("Value of Number is="+number);
+var two=2;                      // var type variable
+console.log("Value of Var Variable is="+two);
+let  three=3;
+console.log("Value of Let Variable is="+three);
+const four=4;
+console.log("Value of Const Variable is="+four);
