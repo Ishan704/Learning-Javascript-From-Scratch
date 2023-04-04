07b625c37ba29bdf33e0ee1a6977d14e36362ab5
ishan <sethiishan19@gmail.com>
2023-04-04 23:16:48 +0530

    Day6

---
+++ Day6/six.js
@@ -0,0 +1,24 @@
+'use strict';
+var msg="hii";
+function say(){
+    var msg="Hello";
+    console.log(msg);
+}
+say();
+console.log(msg);
+
+
+var dta="hi";
+function datar(){
+    console.log(dta);
+}
+datar()
+
+
+function gecounter(){
+    counter=19;
+    return counter;
+
+}
+console.log(gecounter());
+
