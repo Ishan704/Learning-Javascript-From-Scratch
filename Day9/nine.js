b075f86cc098ba3dc71e95e4a47b5c48579a06aa
ishan <sethiishan19@gmail.com>
2023-04-09 19:28:24 +0530

    Day9

---
+++ Day9/nine.js
@@ -0,0 +1,31 @@
+// Display a text five times
+for(let i=1;i<=5;i++)
+console.log("I love JS");
+
+//Display number from 1 to 5
+console.log("Next")
+for(let i=1;i<=5;i++)
+console.log(i);
+
+// loop that has no intializer expression
+console.log("Next")
+let j=1;
+for(;j<5;j++)
+console.log(j);
+
+//loop without the condition example
+console.log("Next");
+for(let j=1;;j+=2)
+{
+    console.log(j);
+    if(j>10)
+    break;
+}
+console.log("Next")
+//Infinite for loop
+//for(let i=1;i>0;i++){
+//
+//}
+
+
+ 
