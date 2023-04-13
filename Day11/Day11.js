e635bd17736c540a50d4d16c4277e3c227d946ea
ishan <sethiishan19@gmail.com>
2023-04-13 23:31:58 +0530

    11

---
+++ Day11/Day11.js
@@ -0,0 +1,10 @@
+//Concat()=This method is used for merging
+const arr1=[1,2,3,4];//array
+const arr2=[5,6,7,8];
+const arr3=arr1.concat(arr2);
+console.log(arr3);
+ 
+//push()=This method adds one or more elements to the end of the array and returns the new length of the array
+const numbers=[1,2,3,4,5,6];
+numbers.push(7);
+num
