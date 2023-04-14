3ac63107c8cf69e151438a0785040a552946bcf0
ishan <sethiishan19@gmail.com>
2023-04-14 23:29:13 +0530

    Day11

--- Day11/Day11.js
+++ Day11/Day11.js
@@ -1,10 +1,36 @@
-//Concat()=This method is used for merging
-const arr1=[1,2,3,4];//array
-const arr2=[5,6,7,8];
-const arr3=arr1.concat(arr2);
-console.log(arr3);
+//findindex() method returns the index of the first element in array that satisifies the provided testing function if nk elements satisfy the testing function -1 is returned
+
+const a1=[1,2,3,4,5];
+const islarge=(val)=>val>13;
+console.log(a1.findIndex(islarge));
+
+const isgreater=(points)=>points<20;
+console.log(a1.findIndex(isgreater));   //findindex() method
+
+const isbelow=(value)=>value>0;
+ console.log(a1.every(isbelow));  // every() method
  
-//push()=This method adds one or more elements to the end of the array and returns the new length of the array
-const numbers=[1,2,3,4,5,6];
-numbers.push(7);
-num
+ const a2=[1,3,5,7];
+ const isany=(value)=>value%2==1;  //some()  method
+ console.log(a2.some(isany));
+
+
+ console.log(a2.join());
+ console.log(a1.join('-'))  //join() method
+
+
+ console.log(a2.includes(11));
+
+
+ const reversed=a2.reverse();
+ console.log(reversed);
+
+
+ a2.forEach(ele=>console.log(ele));
+
+ const map1=a1.map(r=>r*2);
+ console.log(map1);
+
+ let daily=['ea','ean'];
+ console.log(daily.sort());
+ console.log(daily.indexOf('ea'));
