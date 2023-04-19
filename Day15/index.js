74e1abd924f03926163978cf889ff64ab5dad2f2
ishan <sethiishan19@gmail.com>
2023-04-19 23:40:21 +0530

    practice js

---
+++ Day15/index.js
@@ -0,0 +1,27 @@
+console.log("hello");
+let price=100;
+if(price>50)
+console.log("congrats1");
+
+
+if(price>100)
+ console.log("congrats1");
+console.log("congrats2");
+console.log("congrats3");
+
+if(price>10);
+console.log("congrats1");
+
+
+let mrp=1000;
+if(mrp>100)
+{
+    console.log("1");
+}
+else if(mrp>400)
+{
+    console.log("2");
+}
+else{
+    console.log("3");
+}
